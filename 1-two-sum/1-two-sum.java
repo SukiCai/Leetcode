683c0ca6e7dea8aae4d9a684b3a6eb06f793b080
SukiCai <78617799+SukiCai@users.noreply.github.com>
2021-11-04 12:53:47 -0700

    Time: 47 ms (37.48%), Space: 39.4 MB (59.38%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,11 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
-        
+        if (nums.length == 0) {
+            return result;
+        }
         for (int i = 0; i < nums.length - 1; i++) {
-            int left = target - nums[i];
-            for (int j = i + 1; j < nums.length; j++) {
-                if (left == nums[j]){
+            for (int j = i+1; j < nums.length; j++) {
+                if (target - nums[i] == nums[j]) {
                     result[0] = i;
                     result[1] = j;
                 }
@@ -14,3 +15,20 @@ class Solution {
         return result;
     }
 }
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int[] result = new int[2];
+        
+//         for (int i = 0; i < nums.length - 1; i++) {
+//             int left = target - nums[i];
+//             for (int j = i + 1; j < nums.length; j++) {
+//                 if (left == nums[j]){
+//                     result[0] = i;
+//                     result[1] = j;
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// }
